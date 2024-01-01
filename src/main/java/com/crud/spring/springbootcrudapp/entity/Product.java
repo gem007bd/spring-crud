76fff920b446bf92f5a8434f00b806fbbea36f03
gem007bd <gem007bd@yahoo.com>
2024-01-01 01:53:34 -0800

    update entity

--- src/main/java/com/crud/spring/springbootcrudapp/entity/Product.java
+++ src/main/java/com/crud/spring/springbootcrudapp/entity/Product.java
@@ -22,5 +22,5 @@ public class Product {
     private int quantity;
     private double price;
     private DateFormat created;
-    private DateFormat upDateFormat;
+    private DateFormat updated;
 }
